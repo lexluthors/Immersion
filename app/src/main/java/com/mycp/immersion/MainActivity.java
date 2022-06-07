d3736ba5698fbeb2b6ef02b58d5a66a49b4ddd41
刘杰 <594134383@qq.com>
2022-06-07 22:21:46 +0800

    完善

--- app/src/main/java/com/mycp/immersion/MainActivity.java
+++ app/src/main/java/com/mycp/immersion/MainActivity.java
@@ -7,17 +7,18 @@ import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.gyf.immersionbar.ImmersionBar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
-
-import com.gyf.immersionbar.ImmersionBar;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
@@ -29,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
     float toolbarHeight = 520;
     private Toolbar toolbar;
     private LinearLayout rootLayout;
+    private ImmersionBar immersionBar;
+    private TextView toolbarTitle;
 
     private int navTitleColor = Color.parseColor("#ff0000");
 
@@ -36,11 +39,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImmersionBar.with(this).
-                barColor(R.color.transparent).addViewSupportTransformColor(findViewById(R.id.toolbar)).fitsSystemWindows(false).init();
-
+        immersionBar = ImmersionBar.with(this)
+                .barColor(R.color.transparent)
+                .addViewSupportTransformColor(findViewById(R.id.toolbar))
+                .fitsSystemWindows(false)
+                .statusBarDarkFont(true);
+        immersionBar.init();
 
         statusView = findViewById(R.id.statusView);
+        toolbarTitle = findViewById(R.id.toolbarTitle);
+        toolbarTitle.setText("测试容器");
         Log.e("ces测试", getStatusBarHeight(this)+"高度");
         statusView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,getStatusBarHeight(this)));
         recyclerView = findViewById(R.id.recyclerView);
@@ -59,31 +67,31 @@ public class MainActivity extends AppCompatActivity {
         initColor();
     }
 
+    float alpha = 0f;
+    /**
+     * 是否是透明的
+     */
+    boolean isTrans = false;
 
     private void initColor() {
-        int red = (navTitleColor & 0xff0000) >> 16;
-
-        int green = (navTitleColor & 0x00ff00) >> 8;
-
-        int blue = (navTitleColor & 0x0000ff);
         recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
             public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
                 super.onScrolled(recyclerView, dx, dy);
                 Log.e("ces测试", "dx大小》》》" + dx + "  dy大小》》》" + dy);
                 mDisplayY += dy;
-
                 if (mDisplayY <= toolbarHeight) {
-                    float alpha = mDisplayY / toolbarHeight;
-                    int alpha2 = (int) (alpha * 255);
+                    alpha = mDisplayY / toolbarHeight;
                     Log.e("alpha", "alpha》》》" + alpha);
-//                    toolbar.setAlpha(alpha);
-//                    toolbar.getBackground().setAlpha(alpha2);
                     setAlphaAllView(rootLayout,alpha);
+                    immersionBar.statusBarDarkFont(true,alpha).init();
+                    isTrans = true;
                 } else {
-                    rootLayout.setBackgroundColor(navTitleColor);
-                    rootLayout.setAlpha(1f);
-//                    toolbar.getBackground().setAlpha(255);
+                    if (isTrans){
+                        setAlphaAllView(rootLayout,1);
+                        immersionBar.statusBarDarkFont(false).init();
+                        isTrans = false;
+                    }
                 }
             }
         });
@@ -93,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
      * 设置view 透明度 包括子view
      *
      * @param view
-     * @param alpha 10进制
+     * @param alpha 10进制 0...1
      */
     public static   void setAlphaAllView(View view, float alpha) {
         if (view == null) {
@@ -102,21 +110,24 @@ public class MainActivity extends AppCompatActivity {
         if (view.getBackground() != null) {
             view.getBackground().mutate().setAlpha((int) (alpha * 255));
         }
-        float alphaNum = alpha;
-        Log.d( "asdfa","setAlphaAllView alpha:" + alpha + " alphaNum:" + alphaNum);
-        view.setAlpha(alphaNum);
+        Log.d( "asdfa","setAlphaAllView alpha:" + alpha + " alphaNum:" + alpha);
+        view.setAlpha(alpha);
         //设置子view透明度
         if (view instanceof ViewGroup) {
             ViewGroup vp = (ViewGroup) view;
             for (int i = 0; i < vp.getChildCount(); i++) {
                 View viewChild = vp.getChildAt(i);
-                //调用本身（递归）
+                //（递归）
                 setAlphaAllView(viewChild, alpha);
             }
         }
     }
 
-    //获取状态栏的高度
+    /**
+     * 获取状态栏的高度
+     * @param activity
+     * @return
+     */
     private int getStatusBarHeight(Activity activity) {
         int resourceId = activity.getResources().getIdentifier("status_bar_height", "dimen", "android");
         if (resourceId > 0) {
